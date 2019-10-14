b74add0a81c795465702da418f58541212dbc89a
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-14 09:26:28 -0600

    Get connection to the api and return characters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def index
-    
+    key = ENV['POTTER_API_KEY']
+    conn = Faraday.new(url: 'https://www.potterapi.com/v1', params: { key: key })
+
+    oauth_response = conn.get('characters')
+    characters = JSON.parse(oauth_response.body)
   end
 end
