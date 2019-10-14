0061ff1b98027724bb394c3369d48f6d91c8a1f5
Tyler Schaffer <tpschaffer@gmail.com>
2019-10-14 10:40:35 -0600

    Clean up the view and add missing logic to facade

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -4,16 +4,20 @@ class SearchFacade
   end
 
   def format_characters(house)
-    characters = @potter_service.get_characters(house)
-    binding.pry
-    characters.each do |char_hash|
-      char = Character.new(char_hash['name'], char_hash['house'])
-      if char_hash['role']
-        char.set_role(char_hash['role'])
-      end
-      if char_hash['patronus']
-        char.set_patronus(char_hash['patronus'])
+    all_characters = @potter_service.get_characters(house)
+    selected_characters = []
+    all_characters.each do |char_hash|
+      if char_hash['orderOfThePhoenix']
+        char = Character.new(char_hash['name'], char_hash['house'])
+        if char_hash['role']
+          char.set_role(char_hash['role'])
+        end
+        if char_hash['patronus']
+          char.set_patronus(char_hash['patronus'])
+        end
+        selected_characters << char
       end
     end
+    selected_characters
   end
 end
